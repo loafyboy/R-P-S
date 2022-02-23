05524919c9bc306c35238c58277b7dcf6fb681a6
Matthew <matthewgarcia229@icloud.com>
2022-02-22 22:38:08 -0800

    rps.html and rps.js

--- rps.js
+++ rps.js
@@ -1,49 +1,103 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button')
+const computerCountDisplay = document.getElementById('computerCount')
+const userCountDisplay = document.getElementById("userCount")
+const gameWinnerDisplay = document.getElementById("gameWinner")
+
+
+let userChoice
+let computerChoice
+let result
+let userCount = 0
+let computerCount = 0
+let gameWinner = "???"
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    computerPlay()
+    playRound()
+    upTo5()
+    pickWinner()
+    restartGame()
+}))
+
 function computerPlay(){
-    const arr = ["Rock", "Paper", "Scissors"];
-    let random = arr[Math.floor(Math.random() * arr.length)];
-    return random;
-}
-
-
-function game(){
-
-    function playRound(playerSelection, computerSelection){
-        if (playerSelection == "rock" && computerSelection == "Scissors"){
-            console.log("Rock beats Scissors, You Win!");
-        }
-        else if (playerSelection == "rock" && computerSelection == "Paper"){
-            console.log("Paper beats Rock, You lose");
-        }
-        else if(playerSelection == "rock" && computerSelection == "Rock"){
-            console.log("Rock and Rock, It's a tie");
-       }
-        else if (playerSelection == "paper" && computerSelection == "Rock"){
-            console.log("Paper beats Rock, You win");
-       }
-       else if (playerSelection == "paper" && computerSelection == "Scissors"){
-            console.log("Scissors beats Paper, You lose");
-       }
-       else if (playerSelection == "paper" && computerSelection == "Paper"){
-            console.log("Paper and Paper, It's a tie");
-        }
-        else if (playerSelection == "scissors" && computerSelection == "Paper"){
-            console.log("Scissors beats paper, You win");
-        }
-        else if (playerSelection == "scissors" && computerSelection == "Rock"){
-            console.log("Rock beats scissors, You lose");
-        }
-        else if (playerSelection == "scissors" && computerSelection == "Scissors"){
-            console.log("Scissors and scissors, It's a tie");
-        }
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    if (randomNumber === 1){
+        computerChoice = "rock";
     }
-    let player = prompt("Rock, Paper, Scissors?");
-    const playerSelection = player.toLowerCase();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+    if ( randomNumber === 2) {
+        computerChoice = "scissors";
+    }
+    if (randomNumber === 3){
+        computerChoice = "paper";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-console.log(game());
-console.log(game());
-console.log(game());
-console.log(game());
-console.log(game());
+function playRound(){
+    if (computerChoice === userChoice){
+        result = "It's a tie"
+    }
+    if (computerChoice === "rock" && userChoice === "paper"){
+        result = "You win"
+    }
+    if (computerChoice === "rock" && userChoice === "scissors"){
+        result = "You lose"
+    }
+    if (computerChoice === "paper" && userChoice === "rock"){
+        result = "You lose"
+    }
+    if (computerChoice === "paper" && userChoice === "scissors"){
+        result = "You win"
+    }
+    if (computerChoice === "scissors" && userChoice === "rock"){
+        result = "You win"
+    }
+    if (computerChoice === "scissors" && userChoice === "paper"){
+        result = "You lose"
+    }
+
+    resultDisplay.innerHTML = result;
+}
+
+function upTo5(){
+    if(result === "You win"){
+        userCount += 1
+    }
+    if(result === "You lose"){
+        computerCount += 1
+    }
+    userCountDisplay.innerHTML = userCount
+    computerCountDisplay.innerHTML = computerCount
+}
+
+function pickWinner(){
+    if(userCount == 5){
+        gameWinner = "You"
+    }
+    if(computerCount == 5) {
+        gameWinner = "Computer"
+    }
+    gameWinnerDisplay.innerHTML = gameWinner
+}
+
+function restartGame(){
+    if (userCount == 5){
+        userCount = 0
+        computerCount = 0
+        gameWinner = "???"
+    }
+
+    if (computerCount == 5){
+        computerCount = 0
+        userCount = 0
+        gameWinner = "???"
+    }
+    
+
+}
